284776627e616c835e4a700e25b65ecf13ccd374
Nicolas <pnschlei@gmail.com>
2022-02-11 23:13:01 +0100

    rewrite settings file

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-agxazubqmq0cbp8fi=8n(ou_&)3^m_i=w97#4o1z$2n5sa!%=&'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
